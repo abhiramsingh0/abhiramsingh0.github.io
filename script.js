6fc7362278e00e8db752624edd8c81f9f910011c
abhiramsingh0 <abhiram25.1990@gmail.com>
2020-07-13 00:08:12 +0530

    implemented addition game.

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let num1 = Math.floor(Math.random() * 11);
+    let num2 = Math.floor(Math.random() * 11);
+    document.getElementById("num1").innerHTML = num1;
+    document.getElementById("num2").textContent = num2;
+    let button = document.getElementById("butn")
+    button.disabled = true
+    input = document.getElementById("input")
+    input.value = ''
+    input.addEventListener('keyup', () => {
+        if(document.getElementById("input").value.length > 0){
+            button.disabled = false
+        }
+        else{
+            button.disabled = true
+        }
+    })
+    input.addEventListener('mouseup', () => {
+        if(document.getElementById("input").value.length > 0){
+            button.disabled = false
+        }
+        else{
+            button.disabled = true
+        }
+    })
+
+    button.addEventListener("click", () => {
+        let score = Number(document.getElementById("score").textContent);
+        let answer = Number(document.getElementById("input").value);
+        if (answer == num1+num2){
+            document.getElementById("score").innerHTML = score+1;
+        }
+        else{
+            document.getElementById("score").innerHTML = score-1;
+        }
+        document.getElementById("input").value = ''
+        num1 = Math.floor(Math.random() * 11);
+        num2 = Math.floor(Math.random() * 11);
+        document.getElementById("num1").innerHTML = num1;
+        document.getElementById("num2").textContent = num2;
+        if (score == 4){
+            alert('You won!! Restarting.')
+            document.getElementById("score").innerHTML = 0;
+        }
+        else if (score == -4){
+            alert('You lost!! Restarting.')
+            document.getElementById("score").innerHTML = 0;
+        }
+    })
+}
+)
